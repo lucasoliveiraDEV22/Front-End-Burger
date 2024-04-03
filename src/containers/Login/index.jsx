28b21033ba51e542fc5baee6d65cfc47cd11a362
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2024-04-03 10:37:59 -0300

    new-change

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -9,15 +9,27 @@ import Logo from '../../assets/login.svg'
 import Button from '../../components/Button'
 import { useUser } from '../../hooks/UserContext'
 import api from '../../services/api'
-import { Container, ContainerItems, ErrorMessage, Input, Label, LoginImage, SignInLink } from './styles'
-function Login () {
+import {
+  Container,
+  ContainerItems,
+  ErrorMessage,
+  Input,
+  Label,
+  LoginImage,
+  SignInLink
+} from './styles'
+function Login() {
   const navigate = useNavigate()
 
   const { putUserData } = useUser()
 
   const schema = Yup.object().shape({
-    email: Yup.string().email('Digite um e-mail válido').required('O e-mail é obrigatório'),
-    password: Yup.string().required('A senha é obrigatória').min(6, 'A senha deve ter pelo menos 6 dígitos')
+    email: Yup.string()
+      .email('Digite um e-mail válido')
+      .required('O e-mail é obrigatório'),
+    password: Yup.string()
+      .required('A senha é obrigatória')
+      .min(6, 'A senha deve ter pelo menos 6 dígitos')
   })
 
   const {
@@ -51,24 +63,38 @@ function Login () {
 
   return (
     <Container>
-      <LoginImage src={LoginImg} alt = "login-image" />
+      <LoginImage src={LoginImg} alt="login-image" />
       <ContainerItems>
-
-        <img src = {Logo} alt = "logo" />
+        <img src={Logo} alt="logo" />
         <h1>Login</h1>
         <form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <Label>Email</Label>
-        <Input type='email' {...register('email')} error = {errors.email?.message}/>
-        <ErrorMessage>{errors.email?.message}</ErrorMessage>
+          <Label>Email</Label>
+          <Input
+            type="email"
+            {...register('email')}
+            error={errors.email?.message}
+          />
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
 
-        <Label>Senha</Label>
-        <Input type='password' {...register('password')} error = {errors.password?.message}/>
-        <ErrorMessage>{errors.password?.message}</ErrorMessage>
+          <Label>Senha</Label>
+          <Input
+            type="password"
+            {...register('password')}
+            error={errors.password?.message}
+          />
+          <ErrorMessage>{errors.password?.message}</ErrorMessage>
 
-        <Button type="submit" style={{ marginTop: 30, marginBottom: 10 }}>Sign In</Button>
+          <Button type="submit" style={{ marginTop: 30, marginBottom: 10 }}>
+            Sign In
+          </Button>
         </form>
-        <SignInLink> Não possui conta?{' '}
-        <Link style={{ color: 'white' }} to = '/cadastro'>Sign up</Link> </SignInLink>
+        <SignInLink>
+          {' '}
+          Não possui conta?{' '}
+          <Link style={{ color: 'white' }} to="/cadastro">
+            Sign up
+          </Link>{' '}
+        </SignInLink>
       </ContainerItems>
     </Container>
   )
