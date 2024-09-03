7bc23cc95d411d3e8909d9cccc630e9c4eb00006
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2024-09-03 14:19:41 -0300

    updated

--- src/containers/Admin/styles.js
+++ src/containers/Admin/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   min-height: 100vh;
-  min-width: 100vw;
+  min-width: 100%;
   background: #efefef;
   justify-content: flex-start;
 `
